c4427e99103af767f5611a6d63d35d3ad6e308de
Austin Abbott <magicaustin51547@gmail.com>
2020-05-16 17:21:24 -0600

    Add material ui app bar

--- hello-world/src/components/header.js
+++ hello-world/src/components/header.js
@@ -1,16 +1,56 @@
 import React from "react"
 import { Link } from "gatsby"
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Header() {
+
+  const classes = useStyles();
   return (
-    <div class="header">
-      <Link to="/">My Logo</Link>
-      <div class="header-right">
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
-      </div>
-    </div>
+    // <div class="header">
+    // <AppBar>
+    //   <Link to="/">My Logo</Link>
+    //   <div class="header-right">
+    //     <nav>
+    //       <Link to="/about">About</Link>
+    //       <Link to="/contact">Contact</Link>
+    //     </nav>
+    //     <Button color="primary">Primary</Button>
+    //   </div>
+    //   </AppBar>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+    // </div>
   )
 }
