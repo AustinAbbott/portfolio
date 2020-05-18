28f4d7d3b284cb11e701ee1a604ec22d2e4af1df
Austin Abbott <magicaustin51547@gmail.com>
2020-05-18 14:11:43 -0600

    Add responsive header

--- hello-world/src/components/header.js
+++ hello-world/src/components/header.js
@@ -3,35 +3,73 @@ import { Link } from "gatsby"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
+// for responsive header
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from "@material-ui/core/Paper"
+import Grid from "@material-ui/core/Grid"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}))
 
 export default function Header(props) {
+  const classes = useStyles()
   return (
     <AppBar
       position="static"
-      style={{ background: "#00a9a6 ", padding: "2em" }}
+      style={{
+        background: "#00a9a6 ",
+        padding: "1em",
+        display: "flex",
+        justifyContent: "right",
+      }}
     >
       <Toolbar>
-        <img
-          src="https://i.imgur.com/AnqGLXM.jpg"
-          style={{ width: "15%", height: "auto", borderRadius: "50%" }}
-          alt="Austin Abbott profile"
-        ></img>
-
-        <Button>
-          <Link style={{ color: "white" }} to="/">
-            Home
-          </Link>
-        </Button>
-        <Button>
-          <Link style={{ color: "white" }} to="/about">
-            About
-          </Link>
-        </Button>
-        <Button>
-          <Link style={{ color: "white" }} to="/contact">
-            Contact
-          </Link>
-        </Button>
+        <div className={classes.root}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item xs={12} sm={6}>
+              <div style={{ textAlign: "center" }}>
+                <img
+                  src="https://i.imgur.com/AnqGLXM.jpg"
+                  style={{ width: "15%", height: "auto", borderRadius: "50%" }}
+                  alt="Austin Abbott profile"
+                ></img>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div style={{ textAlign: "center" }}>
+                <Button>
+                  <Link style={{ color: "white" }} to="/">
+                    Home
+                  </Link>
+                </Button>
+                <Button>
+                  <Link style={{ color: "white" }} to="/about">
+                    About
+                  </Link>
+                </Button>
+                <Button>
+                  <Link style={{ color: "white" }} to="/contact">
+                    Contact
+                  </Link>
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </div>
       </Toolbar>
     </AppBar>
   )
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,13 +1,54 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from "@material-ui/core/Paper"
+import Grid from "@material-ui/core/Grid"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}))
 
 export default function Home() {
+  const classes = useStyles()
   return (
     <div>
       <Header />
       <div id="main-homepage">
-      <h1>this is the h1 on the homepage</h1>
+        <h1>this is the h1 on the homepage</h1>
+
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>xs=12</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+          </Grid>
+        </div>
       </div>
       <Footer />
     </div>
