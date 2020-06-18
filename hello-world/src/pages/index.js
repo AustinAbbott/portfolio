9a22dfc768921e2ebcd641ffa3066f6a945499d5
Austin Abbott <magicaustin51547@gmail.com>
2020-06-18 15:14:04 -0600

    Set default text color to more natural dark grey

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,13 +4,22 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <Header></Header>
-      <div style={{textAlign: "center"}}>
-        <p>Home</p>
-        <p>Profile photo here</p>
+      <div>
+        <div style={{ width: "60%" }}>
+          <p>
+            Hi, I'm Austin Abbott. A former Purchasing Agent / Salesperson /
+            Automotive Machinist / Guitar Instructor turned software engineer! I
+            have a passion for quality, pixel-perfect design, and the open web.
+            Below is some of the tech I have used in projects. If you have any
+            questions, please reach out!
+          </p>
+        </div>
         <div class="tech-stack">
-
+          <div class="stack-images">
+            <img src="https://img.icons8.com/color/144/000000/react-native.png" />
+          </div>
           <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/javascript.png" />
           </div>
@@ -21,12 +30,12 @@ export default function Home() {
             <img src="https://img.icons8.com/color/144/000000/css3.png" />
           </div>
           <div class="stack-images">
-          <img src="https://img.icons8.com/color/144/000000/react-native.png"/>
+            <img src="https://img.icons8.com/wired/144/000000/webpack.png" />
           </div>
           <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/bootstrap.png" />
           </div>
-          <div class="stack-images"> 
+          <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/material-ui.png" />
           </div>
           <div class="stack-images">
@@ -41,9 +50,7 @@ export default function Home() {
           <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/npm.png" />
           </div>
-          <div class="stack-images">
-            <img src="https://img.icons8.com/wired/144/000000/webpack.png" />
-          </div>
+ 
           <div class="stack-images">
             <img src="https://img.icons8.com/wired/144/000000/babel.png" />
           </div>
