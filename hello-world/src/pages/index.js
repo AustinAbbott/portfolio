8f891e29e09f62dc15fd42b37adc662186748db6
Austin Abbott <magicaustin51547@gmail.com>
2020-06-18 14:44:30 -0600

    Add CSS flexbox to tech icon area

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -6,48 +6,46 @@ export default function Home() {
   return (
     <div>
       <Header></Header>
-      <div>
+      <div style={{textAlign: "center"}}>
         <p>Home</p>
-
+        <p>Profile photo here</p>
         <div class="tech-stack">
-          <div>
-            <img
-              src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
-              style={{ width: "10%" }}
-            />
-          </div>
-          <div>
+
+          <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/javascript.png" />
           </div>
-          <div>
+          <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/html-5.png" />
           </div>
-          <div>
+          <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/css3.png" />
           </div>
-          <div>
+          <div class="stack-images">
+          <img src="https://img.icons8.com/color/144/000000/react-native.png"/>
+          </div>
+          <div class="stack-images">
             <img src="https://img.icons8.com/color/144/000000/bootstrap.png" />
           </div>
-          <div>
+          <div class="stack-images"> 
             <img src="https://img.icons8.com/color/144/000000/material-ui.png" />
           </div>
-          <div>
+          <div class="stack-images">
+            <img src="https://img.icons8.com/ios-filled/144/000000/jquery.png" />
+          </div>
+          <div class="stack-images">
             <img src="https://img.icons8.com/fluent/144/000000/visual-studio-code-2019.png" />
           </div>
-          <div>
-          <img src="https://img.icons8.com/color/144/000000/nodejs.png"/>
+          <div class="stack-images">
+            <img src="https://img.icons8.com/color/144/000000/nodejs.png" />
           </div>
-          <div>
-          <img src="https://img.icons8.com/color/144/000000/npm.png"/>
+          <div class="stack-images">
+            <img src="https://img.icons8.com/color/144/000000/npm.png" />
           </div>
-          <div>
-          <img src="https://img.icons8.com/ios-filled/144/000000/jquery.png"/>
+          <div class="stack-images">
+            <img src="https://img.icons8.com/wired/144/000000/webpack.png" />
           </div>
-          <div>
-          <img src="https://img.icons8.com/wired/144/000000/webpack.png"/>
-          </div>
-          <div>
-          <img src="https://img.icons8.com/wired/144/000000/babel.png"/>
+          <div class="stack-images">
+            <img src="https://img.icons8.com/wired/144/000000/babel.png" />
           </div>
         </div>
       </div>
