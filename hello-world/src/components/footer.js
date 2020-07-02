28346cd658a986be8cd0abafd59ce0f91c422790
Austin Abbott <magicaustin51547@gmail.com>
2020-07-02 12:21:21 -0600

    Add resume link to footer, add padding to footer images

--- hello-world/src/components/footer.js
+++ hello-world/src/components/footer.js
@@ -16,6 +16,16 @@ export default function Footer() {
             />
           </a>
           <a
+            href="https://docs.google.com/document/d/1pZCYbLgV5cBRWl49yvEACwpdQxTeDglhfhPyyoxLO1g/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/ultraviolet/48/000000/open-resume.png"
+              alt="resume"
+            />
+          </a>
+          <a
             href="https://github.com/austinabbott"
             target="_blank"
             rel="noopener noreferrer"
