84f6b422ceb3e07922fe68ca0130224c8e7cda66
Austin Abbott <57454313+AustinAbbott@users.noreply.github.com>
2023-01-16 15:02:51 -0700

    Make home page sections components

---
+++ Main/src/components/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const About = () => {
+  return (
+    <div className="about-box">
+      <h3>Hi, I'm Austin Abbott.</h3>
+      <p>
+        I'm a Software Engineer working in Fintech with experience in
+        JavaScript, React, Material-UI, HTML, CSS, Node, Express, and
+        PostgreSQL.
+      </p>
+    </div>
+  )
+}
--- Main/src/pages/index.js
+++ Main/src/pages/index.js
@@ -1,84 +1,16 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import { About } from "../components/about"
+import { Skills } from "../components/skills"
 
 export default function Home() {
   return (
     <div className="page">
       <Header />
-      <div>
-        <div className="about-box">
-          <h3>Hi, I'm Austin Abbott.</h3>
-          <p>
-            I'm a Software Engineer working in Fintech with experience in
-            JavaScript, React, Material-UI, HTML, CSS, Node, Express, and
-            PostgreSQL.
-          </p>
-        </div>
-        <h3 style={{ fontSize: "1.75em" }}>Software Skills</h3>
-        <div className="tech-stack">
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/javascript.png"
-            />
-            <p>JavaScript</p>
-          </div>
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/html-5.png"
-            />
-            <p>HTML5</p>
-          </div>
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/css3.png"
-            />
-            <p>CSS3</p>
-          </div>
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/react-native.png"
-            />
-            <p>React</p>
-          </div>
-
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/bootstrap.png"
-            />
-            <p>Bootstrap</p>
-          </div>
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/material-ui.png"
-            />
-            <p>Material UI</p>
-          </div>
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/ios-filled/144/000000/jquery.png"
-            />
-            <p>jQuery</p>
-          </div>
-          <div className="stack-images">
-            <img
-              alt=""
-              src="https://img.icons8.com/color/144/000000/nodejs.png"
-            />
-            <p>Nodejs</p>
-          </div>
-          <div className="stack-images">
-            <img alt="" src="https://img.icons8.com/color/144/000000/npm.png" />
-            <p>npm</p>
-          </div>
-        </div>
+      <div className="page-content">
+      <About />
+      <Skills />
       </div>
       <Footer />
     </div>
